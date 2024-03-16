ebce5850408fb3123f10e6c9863eec4d10d21d34
danielhsu-m1pro <danielhsu021202>
2024-03-16 01:46:29 -0700

    UI: File import to file tree, open files in table view

---
+++ FileViewer.py
@@ -0,0 +1,156 @@
+
+from PyQt6.QtWidgets import QTableView, QWidget, QFileDialog, QTreeWidgetItem
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QAbstractTableModel, Qt
+
+import os
+import sys
+import re
+import pandas as pd
+
+from ui.FileViewerWidget import Ui_FileViewer
+
+class PandasTableModel(QAbstractTableModel): 
+    def __init__(self, df=pd.DataFrame(), parent=None): 
+        super().__init__(parent)
+        self._df = df
+
+    def toDataFrame(self):
+        return self._df
+
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+        if role != Qt.ItemDataRole.DisplayRole:
+            return None
+
+        if orientation == Qt.Orientation.Horizontal:
+            try:
+                return self._df.columns.tolist()[section]
+            except IndexError:
+                return None
+        elif orientation == Qt.Orientation.Vertical:
+            try:
+                return self._df.index.tolist()[section]
+            except IndexError:
+                return None
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if role != Qt.ItemDataRole.DisplayRole:
+            return None
+
+        if not index.isValid():
+            return None
+
+        return str(self._df.iloc[index.row(), index.column()])
+
+    def setData(self, index, value, role):
+        row = self._df.index[index.row()]
+        col = self._df.columns[index.column()]
+        if hasattr(value, 'toPyObject'):
+            # PyQt6 gets a QVariant
+            value = value.toPyObject()
+        else:
+            # PySide6 gets an unicode
+            dtype = self._df[col].dtype
+            if dtype != object:
+                value = None if value == '' else dtype.type(value)
+        self._df.at[row, col] = value
+        return True
+
+    def rowCount(self, parent=None): 
+        return len(self._df.index)
+
+    def columnCount(self, parent=None): 
+        return len(self._df.columns)
+
+    def sort(self, column, order):
+        colname = self._df.columns.tolist()[column]
+        self.layoutAboutToBeChanged.emit()
+        self._df.sort_values(colname, ascending=order == Qt.SortOrder.AscendingOrder, inplace=True)
+        self._df.reset_index(inplace=True, drop=True)
+        self.layoutChanged.emit()
+
+
+class PandasTable(QTableView):
+    def __init__(self, data, parent=None):
+        super(PandasTable, self).__init__(parent)
+        self.df = data
+        self.model = PandasTableModel(self.df)
+        self.setModel(self.model)
+
+
+
+        
+
+class FileViewer(QWidget, Ui_FileViewer):
+    def __init__(self, parent=None):
+        super(FileViewer, self).__init__(parent)
+        self.setupUi(self)
+
+        self.directories = set()
+        self.df = pd.DataFrame()
+
+        self.mapButtonActions()
+        self.fileTree.itemDoubleClicked.connect(self.openFile)
+
+    def mapButtonActions(self):
+        self.fileTreeImportButton.clicked.connect(self.importDirectory)
+        self.refreshFileTreeButton.clicked.connect(self.refreshFileTree)
+
+
+    def importDirectory(self):
+        """Opens a file dialog to import a directory."""
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if directory:
+            self.directories.add(directory)
+            self.refreshFileTree()
+
+    def refreshFileTree(self):
+        """Refreshes the file tree view with the current directories."""
+        self.fileTree.clear()
+        for directory in self.directories:
+            if os.path.isdir(directory):
+                self.populateFileTree(directory, self.fileTree.invisibleRootItem())
+            else:
+                self.directories.remove(directory)
+
+    def naturalSort(self, l):
+        """Sorts a list of strings in natural order."""
+        convert = lambda text: int(text) if text.isdigit() else text.lower()
+        alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+        return sorted(l, key=alphanum_key)
+        
+    def populateFileTree(self, base_path, parent_item):
+        """
+        Recursively populates a QTreeView with the hierarchical structure of files and directories.
+        """
+        # Create a QTreeWidgetItem for the root directory
+        root_item = QTreeWidgetItem(parent_item)
+        root_item.setText(0, os.path.basename(base_path))  # Set the text for the root item
+        root_item.setIcon(0, QIcon("icons/folder.png"))
+
+        for item in self.naturalSort(os.listdir(base_path)):
+            item_path = os.path.join(base_path, item)
+            if os.path.isdir(item_path):
+                # Recursively populate the tree view with the contents of the directory
+                self.populateFileTree(item_path, root_item)
+            elif os.path.isfile(item_path):
+                # Create a QTreeWidgetItem for files
+                file_item = QTreeWidgetItem(root_item)
+                file_item.setText(0, item)  # Set the text for the item
+                file_item.setData(0, Qt.ItemDataRole.UserRole, item_path)  # Set the data for the item
+
+    def openFile(self, item):
+        """Opens the file and displays it in the table view, replacing the current widget."""
+        if item.childCount() == 0:
+            self.df = pd.read_csv(item.data(0, Qt.ItemDataRole.UserRole))
+            self.df.columns = [str(i) for i in range(len(self.df.columns))]
+            pd_table = PandasTable(self.df)
+            self.dataLayout.replaceWidget(self.dataLayout.itemAt(0).widget(), pd_table)
+
+
+    
+                
+        
+
+
+
